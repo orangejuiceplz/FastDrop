a41e76803891a50d2a67fc82fafc8b024a9b09b8
Mace <127msafran@frhsd.com>
2026-01-17 16:27:25 -0500

    Start share page

--- src/pages/Share.tsx
+++ src/pages/Share.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
 import '../css/share.css'
 import Navbar from '../components/navbar.tsx';
 
 function Share() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Navbar />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='connect-container'>
+        <h1>Connect to another user</h1>
+        <h2>Click the button below to start the connection process</h2>
+        <button className='connect-button'>Connect</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
