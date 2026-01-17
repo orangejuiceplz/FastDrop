42d5795a8d60730c446fd1a933abb8890bb0e4e1
λamdashi <orangejuiceplz12321@gmail.com>
2026-01-17 14:00:07 -0500

    move stuff int backend so it's ready for it to be merged

--- backend/src/code_gen.cpp
+++ backend/src/code_gen.cpp
@@ -10,14 +10,14 @@ namespace fastdrop {
 
     std::string genCode() {
         std::vector<std::string> adjectives = {"cool", "lazy", "git", "charming", "sad", "happy"};
-        std::vector<std::string> nouns1 = {"octopus", "snake", "cat", "hippo", "goat", "sheep"};
+        std::vector<std::string> nouns = {"octopus", "snake", "cat", "hippo", "goat", "sheep"};
         std::vector<std::string> stringNum = {"67", "29", "19", "94", "10", "39"};
 
         std::random_device rd;
         std::mt19937 gen(rd());
 
         return adjectives[gen() % adjectives.size()] + "-" +
-               nouns1[gen() % nouns1.size()] + "-" +
+               nouns[gen() % nouns.size()] + "-" +
                stringNum[gen() % stringNum.size()];
     }
 
