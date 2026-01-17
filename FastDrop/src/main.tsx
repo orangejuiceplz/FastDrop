a42c0be28a46b98d71b51f3dac4df00201738980
Mace <127msafran@frhsd.com>
2026-01-17 12:37:53 -0500

    Initial stuff

--- FastDrop/src/main.tsx
+++ FastDrop/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Welcome from './pages/Welcome.tsx';
 import './index.css'
-import App from './App.tsx'
+import App from './pages/Share.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/app" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
