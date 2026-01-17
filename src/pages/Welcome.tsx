e86837cba244c2b72ffa36c0e15aa0757a44872d
Mace <127msafran@frhsd.com>
2026-01-17 16:10:18 -0500

    work on welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -5,8 +5,15 @@ export default function Welcome() {
     return (
         <>
             <Navbar />
-            <h1>Welcome to FastDrop</h1>
-            <h2>A fast and secure file sharing platform</h2>
+            <div className='intro'>
+                <div className='intro-text'>
+                    <h1>Welcome to FastDrop</h1>
+                    <h2>A fast and secure file sharing platform</h2>
+                </div>
+                <div className='image-container'>
+                    <img src="https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=" alt="Intro Image" className='intro-image' />
+                </div>
+            </div>
         </>
     );
 }
